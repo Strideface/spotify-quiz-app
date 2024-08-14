8e6b97f505dcd25e103deb1a801c3d5a3e5d13ae
Strideface <77002479+Strideface@users.noreply.github.com>
2024-08-14 13:00:08 +0100

    Update CustomQuiz.jsx
    - converted my custom modal to the Next UI modal and styled it according to mobile and medium sized screens

--- src/components/stages/play-quiz-stage/components/CustomQuiz.jsx
+++ src/components/stages/play-quiz-stage/components/CustomQuiz.jsx
@@ -1,97 +1,143 @@
 import { useRef, useEffect, useState } from "react";
 import Quiz from "./Quiz";
-import Modal from "../../../Modal";
 import { useOutletContext } from "react-router-dom";
 import LoadingIndicator from "../../../LoadingIndicator";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/button";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
 
 // present an introductory modal when component is rendered initially that explains how to play.
 // Rules change depending on selected difficulty
-// closes once button is presed
+// closes once 'Play' button is presed
 
 export default function CustomQuiz() {
   const { quizData } = useOutletContext();
-  const rulesModal = useRef();
   const [inPlay, setInPlay] = useState(false);
-  // eslint-disable-next-line no-unused-vars
   const [tracksReady, setTracksReady] = useState(false);
 
-  // only open this modal upon initialization, once
-  useEffect(() => {
-    rulesModal.current.open();
-  }, []);
 
   const easy = (
     <ul>
-      <li>Difficuly Selected: Easy</li>
-      <li>Time Limt per track: Unlimited</li>
-      <li>Repeat track limit: Unlimited</li>
+      <li>
+        Difficuly Selected:{" "}
+        <span className=" text-primary underline">Easy</span>
+      </li>
+      <li>
+        Time Limt per track: <span className=" underline">Unlimited</span>
+      </li>
+      <li>
+        Repeat track limit: <span className=" underline">Unlimited</span>
+      </li>
       <li>The whole track will play from the start</li>
     </ul>
   );
 
   const medium = (
     <ul>
-      <li>Difficuly Selected: Medium</li>
-      <li>Time Limt per track: 30 seconds</li>
-      <li>Repeat track from start limit: Unlimited</li>
+      <li>
+        Difficuly Selected:{" "}
+        <span className=" text-orange-500 underline">Medium</span>
+      </li>
+      <li>
+        Time Limt per track: <span className=" underline">30 seconds</span>
+      </li>
+      <li>
+        Repeat track from start limit:{" "}
+        <span className=" underline">Unlimited</span>
+      </li>
       <li>A 30 second preview of the track can be played</li>
     </ul>
   );
 
   const hard = (
     <ul>
-      <li>Difficuly Selected: Hard</li>
-      <li>Time Limt per track: 30 seconds</li>
-      <li>Repeat track from start limit: Unlimited</li>
+      <li>
+        Difficuly Selected:{" "}
+        <span className=" text-red-600 underline">Hard</span>
+      </li>
+      <li>
+        Time Limt per track: <span className=" underline">30 seconds</span>
+      </li>
+      <li>
+        Repeat track from start limit:{" "}
+        <span className=" underline">Unlimited</span>
+      </li>
       <li>A 10 second intro of the track can be played</li>
     </ul>
   );
 
   const handleOnClick = () => {
-    rulesModal.current.close();
     setInPlay(true);
   };
 
   return (
     <>
-      <h1>Custom Quiz</h1>
       <Modal
-        ref={rulesModal}
-        title="Custom Quiz Rules"
-        message="Before starting the quiz, please check how to play and the rules"
+        isOpen={!inPlay}
+        hideCloseButton
+        isDismissable
+        backdrop="blur"
+        classNames={{
+          body: "flex-col justify-center space-y-2 text-mobile-2 md:text-sm-screen-1",
+          footer: " justify-center text-mobile-2 md:text-sm-screen-1",
+          header: "text-mobile-3 md:text-sm-screen-2"
+        }}
       >
-        <div className="flex flex-col justify-center space-x-2">
-          <ul>
-            <li>
-              Identify the artist of the track first by searching the name and
-              then selecting an option.
-            </li>
-            <li>
-              Pick the track by selecting an option from the next dropdown
-              selection field.
-            </li>
-            <li>
-              Submit your answer or Skip if you don't know (An Artist and Track
-              must be selected to submit an answer).
-            </li>
-          </ul>
-          <h2>Rules:</h2>
-          {quizData.current.difficulty === "easy" && easy}
-          {quizData.current.difficulty === "medium" && medium}
-          {quizData.current.difficulty === "hard" && hard}
-        </div>
-        <div className="flex flex-col justify-center space-x-2">
-          <p>The quiz will start as soon as you hit 'Play'!</p>
-          {tracksReady ? (
-            <button type="button" onClick={handleOnClick}>
-              Play
-            </button>
-          ) : (
-            <LoadingIndicator loadingMessage="Preparing Tracks..."/>
-          )}
-        </div>
+        <ModalContent>
+          <ModalHeader>Quiz Rules</ModalHeader>
+          <ModalBody>
+            <div>
+              <ul className=" list-disc space-y-2">
+                <li>
+                  Identify the artist of the track first by searching the name
+                  and then selecting an option.
+                </li>
+                <li>
+                  Pick the track by selecting an option from the next dropdown
+                  selection field.
+                </li>
+                <li>
+                  Submit your answer or Skip if you don't know (An Artist and
+                  Track must be selected to submit an answer).
+                </li>
+              </ul>
+            </div>
+            <div>
+              <Card
+                classNames={{
+                  base: " bg-default-500 text-secondary font-medium",
+                }}
+              >
+                <CardBody>
+                  {quizData.current.difficulty === "easy" && easy}
+                  {quizData.current.difficulty === "medium" && medium}
+                  {quizData.current.difficulty === "hard" && hard}
+                </CardBody>
+              </Card>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <div className="flex flex-col justify-center space-y-2">
+              <p>The quiz will start as soon as you hit 'Play'!</p>
+              <Button
+                color="success"
+                onPress={handleOnClick}
+                isLoading={!tracksReady}
+              >
+                Play
+              </Button>
+            </div>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
-      <Quiz inPlay={inPlay} setTracksReady={setTracksReady}/>
+
+      <Quiz inPlay={inPlay} setTracksReady={setTracksReady} />
     </>
   );
 }
